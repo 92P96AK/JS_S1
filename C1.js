4573d27c8ab3cb90250b98a0345593f285c46801
Pradip Kharal <41242627+pkboss777@users.noreply.github.com>
2019-02-10 01:17:47 +0545

    Update C1.js

--- C1.js
+++ C1.js
@@ -1,8 +1,8 @@
 // js code for removing extra spaces from given string to a function and it also create string that has 
 // every first character of string toUpperCase() and others toLowerCase()
 // It mightbe applicable for database systems since we have to search exactly the same string as we put during storing 
-// So during storing and searching time we just need to call this function
-// It will output the same for PRADIP KHARAL,   PRADIP KHARAL, PradiP Kharal,PrAdIp         kharaL,.... etc. to Pradip Kharal
+// So during storing and searching time you just need to call the function
+// It will output the same for PRADIP KHARAL,   PRADIP KHARAL, PradiP Kharal,PrAdIp         khAraL,.... etc. to Pradip Kharal
 // you can call this function simply searchoptimization('your string goes here').
 
 
