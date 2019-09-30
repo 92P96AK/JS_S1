a0a752cadab5f5c21e68a82b7b1047680283c22b
Pradip Kharal <41242627+pkboss777@users.noreply.github.com>
2019-09-30 10:55:48 +0545

    Update C4.js

--- C4.js
+++ C4.js
@@ -2,6 +2,7 @@
 // for example we have array1 and array2 given as below 
 // now we need to remove all element of array 2 from array1 
 // then this might be helpful using es6
+// it gives new array of [1,2,3] for given below case
 
 let array1=[1,2,3,4,5]
 let array2=[4,5,6,7]
